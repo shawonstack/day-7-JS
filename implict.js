91274f685e550e1f50ab60986835dfcd0a5985dc
shawonstack <shawon934764@gmail.com>
2026-06-11 03:35:48 +0600

    types of implict return in silgle valuse from arrow function

---
+++ implict.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+console.log(add(2, 4));
+const isAdult = age => age >= 18;
+console.log(isAdult(66));
+const numbers = [1, 2, 3, 4, 5];
+
+const result = numbers.filter(num => num > 3);
+
+console.log(result);
