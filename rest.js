b20d4271c8b3e171e7ce9e6881f62c525b4502b2
shawonstack <shawon934764@gmail.com>
2026-06-19 12:18:31 +0600

    rest perameter sum of the number solving

--- rest.js
+++ rest.js
@@ -1,4 +1,38 @@
-function add(...numbers) {
-  console.log(numbers);
+// // function add(...numbers) {
+// //   console.log(numbers);
+// // }
+// // add(12, 13, 14);
+// // ----------
+// function sum(...numbers) {
+//   let total = 0;
+
+//   for (let number of numbers) {
+//     total += number;
+//   }
+
+//   return total;
+// }
+
+// // console.log(sum(10, 20));
+// // console.log(sum(10, 20, 30));
+// // console.log(sum(10, 20, 30, 40));
+
+// function allCustomer(...allnumbers) {
+//   console.log(allnumbers);
+// }
+// allCustomer('shawon', 'rafi', 'hasan');
+// // ------------------------
+function add(...a) {
+  console.log(a);
 }
-add(12, 13, 14);
+add(12, 14, 34, 65);
+// sum of the numebr using rest parameter
+function sum(...numbers) {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return ('total numbers is', total);
+}
+console.log('numbers of sum is:', sum(10, 20, 30));
+// -----------product count
