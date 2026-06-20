a7bf38f7dc8b906636875c9d976390d4c83b1830
shawonstack <shawon934764@gmail.com>
2026-06-20 22:20:49 +0600

    speard operator using array merge ecommerce site

--- speard.js
+++ speard.js
@@ -6,3 +6,24 @@ console.log(addNumbers(...numbers));
 // --------------
 let another = [1, ...numbers, 3, 5];
 console.log(another);
+// ------------ concat insted of speard
+let number1 = [1, 2, 3];
+let number2 = [4, 5, 6];
+let fnalnumber = [...number1, ...number2];
+console.log(fnalnumber);
+// array coppy uisng speard operator
+let array = ['mango', 'apple', 'banana'];
+let copy = [...array];
+console.log(copy);
+// ------------ puash something----------------
+let fruit = ['lichy', 'banana', 'apple', 'orrange'];
+let newFruit = [...fruit];
+newFruit.push('amloki');
+console.log(newFruit);
+// --------------
+const products = ['Laptop', 'Mouse'];
+
+const newProducts = [...products];
+
+newProducts.push('Keyboard');
+console.log(newProducts);
